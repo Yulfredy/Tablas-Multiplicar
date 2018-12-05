f69f247a0ec15936fbe75713f1879f88e188708c
YULFREDY <yulfredyrodriguez@gmail.com>
2018-12-05 10:50:31 -0500

    commit inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let base = 5;
+let datos = '';
+
+
+for (i = 0; i <= 10; i++) {
+    datos += `${base}*${i}= ${base*i}\n`;
+}
+
+const data = new Uint8Array(Buffer.from(datos));
+fs.writeFile(`tablas/tabla del ${base}.txt`, data, (err) => {
+    if (err) throw err;
+    console.log('Archivo creado!');
+});
